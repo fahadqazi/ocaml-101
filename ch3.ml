ac54dac4555225abc1f7761f39c95ed8cc8eefaa
Fahad Qazi <fahadqazi@gmail.com>
2017-06-18 01:13:16 +0100

    completed exercise for chapter 3

--- ch3.ml
+++ ch3.ml
@@ -39,4 +39,68 @@ let rec gcd a b =
 
 gcd 15 9;;
 
+let rec gcd2 a b =
+  match b with
+    0 -> a
+  | _ -> gcd b (a mod b);;
 
+gcd 42 14;;
+
+(* Exercises *)
+
+(* 1. Rewrite the not function from the previous chapter in pattern matching style.  *)
+let not x =
+  if x then false else true;;
+
+let not x =
+  match x with
+    false -> true
+  | true -> false;;
+
+(* 2. Use pattern matching to write a recursive function which, given a 
+positive integer n, returns the sum of all the integers from 1 to n. *)
+
+let rec sum n =
+  if n = 1 then 1 else n + sum (n - 1);;
+
+let rec sum n =
+  match n with
+    1 -> 1
+  | _ -> n + sum (n-1);;
+
+(* 3. Use pattern matching to write a function which, given two numbers x 
+and n, computes x** n *)
+
+let rec power x n =
+  if n = 0 then 1 else
+  if n = 1 then x else
+    x * power x (n-1);;
+
+  
+let rec powers x n =
+  match n with
+    0 -> 1
+  | 1 -> x
+  | _ -> x * powers n x-1;;
+
+(* 5. What does match 1 + 1 with 2 -> match 2 + 2 with 3 -> 4 | 4 -> 5 evaluate to? *)
+
+(* match 1 + 2 with *)
+(*   2 -> (match 2 + 2 with *)
+(*       3 -> 4 *)
+(*     | 4 -> 5 ) *)
+
+
+(* 6. There is a special pattern x..y to denote continuous ranges of characters,
+ for example 'a'..'z' will match all lowercase letters. Write functions islower
+   and isupper, each of type char → bool, to decide on the case of a given letter *)
+
+let isLower char =
+  match char with
+  'a'..'z' -> true
+  | _ -> false
+
+let isUpper char =
+  match char with
+  'A'..'Z' -> true
+  | _ -> false
